479c39b82dd7e98e5d735482c56dd67f3fdab13c
mehdi NGT system <mehdirajabi.NGT@gmail.com>
2023-10-25 16:25:05 +0330

    init

---
+++ src/main/java/com/example/testpaas/TestPaaSApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPaaSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPaaSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpaas/TestPaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPaaSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
